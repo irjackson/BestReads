546569040cc9f000ded29ec062d363d0690b530d
Ian Jackson <ianjackson@Ians-MacBook-Pro.local>
2022-06-29 12:03:17 -0700

    signup/login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,20 +4,52 @@ import './Login.css'
 // import img from '../../images/BestReads.png';
 
 const Login = () => {
+  const dummyData = [
+    {
+      username: 'test',
+      password: 'test'
+    }
+  ]
+  const handleSubmit = (e) => {
+    // prevents page from reloading
+    e.preventDefault();
+    // find user login information
+    const userLogin = dummyData.find((user) => user.username === uname.value)
+    // if username exists in database
+    if (userLogin) {
+      // if password doesn't match up
+      if (userLogin.password !== pass.value) {
+        console.log('invalid username/password input')
+      }
+      // if password does match up
+      else {
+        console.log('')
+      }
+    }
+    // if username doesn't exist in database
+    else {
+      console.log('invalid username/password input')
+    }
+  }
     return (
       <div id='outerBox'>
-      <div id='loginBox'>
-        <label><strong>Log In</strong></label><br/>
-        <input type="text" placeholder="Username " required></input> 
-        <input type="password" placeholder="Password" required></input>
-        <Link to='/app'>
-            <button>Login</button>
-        </Link>
-        <Link to='/signup' id='signuplink'>
+        <form onSubmit={handleSubmit}>
+        <div id='outerBox'>
+          <div id='loginBox'>
+            <label><strong>Log In</strong><br/><br/></label>
+            <input type="text" placeholder="Username" name="uname" required></input> 
+            <input type="password" placeholder="Password" name="pass" required></input>
+            <Link to='/app'>
+            <button type="submit">Login</button>
+            </Link>
+            <Link to='/signup' id='signuplink'>
             <p>Sign up?</p>
-        </Link>
-      </div>
-      </div>
+            </Link>
+          </div>
+        </div>
+        </form>
+        </div>
+      
     );
 }
 
